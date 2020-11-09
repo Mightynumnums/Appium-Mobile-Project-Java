7bbf707e606021396beaaca7df087ba1efe0b103
Aleksandra Shineleva <aleksandrashineleva@sl-0465.lan>
2020-11-09 12:37:11 -0800

    Adds a class for running a test on RDC

--- src/test/java/RDC_Simple_Test.java
+++ src/test/java/RDC_Simple_Test.java
@@ -1,7 +1,5 @@
 import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
-import java.net.URL;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,11 +9,14 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 
-public class SampleSauceTest {
+
+public class RDC_Simple_Test {
 
     private static final String sauceUser = System.getenv("SAUCE_USERNAME");
     private static final String sauceKey = System.getenv("SAUCE_ACCESS_KEY");
+    // This is the app stored in the SL storage
     String APP = "storage:9982ef38-4770-4bd7-88f4-a605d901e155";
     String url = "http://"+sauceUser+":"+sauceKey+"@ondemand.us-west-1.saucelabs.com/wd/hub";
 
@@ -25,20 +26,19 @@ public class SampleSauceTest {
     public void  setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("appiumVersion", "1.18.1");
-        capabilities.setCapability("deviceName","Samsung Galaxy S8 GoogleAPI Emulator");
         capabilities.setCapability("deviceOrientation", "portrait");
-        capabilities.setCapability("browserName", "");
-        capabilities.setCapability("platformVersion","8.1");
+        capabilities.setCapability("browserName", "chrome");
+        // You can set the platformVersion to a specific device OR tell it to just run whatever device is available for a platformVersion and platformName specified.
+        //capabilities.setCapability("deviceName","Google_Pixel_3a_real");
+        capabilities.setCapability("platformVersion","10");
         capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("appPackage", "com.swaglabsmobileapp");
-        capabilities.setCapability("appActivity","com.swaglabsmobileapp.MainActivity");
         capabilities.setCapability("app", APP );
         driver =  new AndroidDriver(new URL(url), capabilities);
         try { Thread.sleep(3000); } catch (Exception ign) {}
     }
 
-        @After
-            public void tearDown() {
+    @After
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
@@ -47,6 +47,8 @@ public class SampleSauceTest {
     @Test
     public void test() {
         WebDriverWait wait = new WebDriverWait(driver, 10);
+        // If you want to have the simplest test EVER, just run the get method for a url (ex below) and nothing else.
+//        driver.get("https://www.google.com/");
         WebElement userNameInput = wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("test-Username")));
         userNameInput.sendKeys("standard_user");
         WebElement passwordInput = driver.findElement(MobileBy.AccessibilityId("test-Password"));
